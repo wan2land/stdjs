9ec37d4a2e70e0eaf62c7dc7d9f599a3540d2663
Cris <corgidisco@gmail.com>
2018-07-03 16:15:52 +0900

    add interfaces to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@
 import {MysqlConnection} from "./driver/mysql/connection"
 import {MysqlPoolConnection} from "./driver/mysql/pool-connection"
 import {Connection} from "./interfaces/interfaces"
+import {
+  ConnectionConfig,
+  MysqlConnectionConfig,
+  MysqlPoolConnectionConfig,
+} from "./interfaces/config"
+import {MysqlResult} from "./interfaces/mysql"
 import {create} from "./create"
 
 export {
@@ -9,4 +15,8 @@ export {
   MysqlConnection,
   MysqlPoolConnection,
   Connection,
+  ConnectionConfig,
+  MysqlConnectionConfig,
+  MysqlPoolConnectionConfig,
+  MysqlResult,
 }
