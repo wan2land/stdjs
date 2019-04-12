68f69a7534160358ce817b2704b07781bd488e3c
Cris <corgidisco@gmail.com>
2019-04-12 09:37:22 +0900

    add exports to index, release v0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 
 export * from "./interfaces/oauth"
+
+export { FacebookStrategy } from "./strategies/facebook"
+export { GithubStrategy } from "./strategies/github"
+export { GoogleStrategy } from "./strategies/google"
+export { KakaoStrategy } from "./strategies/kakao"
+export { NaverStrategy } from "./strategies/naver"
