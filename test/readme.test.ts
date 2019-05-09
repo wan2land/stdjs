aa38c2f6d4ee61ce606b6a5e645c6a92258d257c
Cris <corgidisco@gmail.com>
2019-05-09 13:49:20 +0900

    refactoring test

--- test/readme.test.ts
+++ test/readme.test.ts
@@ -1,26 +1,12 @@
 import * as di from "../lib"
+import { sleep, startConsoleCapture, endConsoleCapture } from "./utils"
 
-function sleep(ms: number): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-// monkey patch
-const consoleLog = console.log
-function turnConsoleOff(): void {
-  console.log = () => { /* nothing */ }
-}
-function turnConsoleOn(): void {
-  console.log = consoleLog
-}
-
-
-describe("tests for README", () => {
+describe("testsuite for README", () => {
   it("test to bind simple value", async () => {
-    expect.assertions(4)
-
     const container = di.create()
 
-    turnConsoleOff()
+    startConsoleCapture()
+
     // section:bind-simple-value
     container.instance("obj1", {message: "this is obj1"})
     container.instance("obj2", {message: "this is obj2"})
@@ -31,21 +17,20 @@ describe("tests for README", () => {
     console.log(await container.get("obj1") === await container.get("obj1")) // true
     console.log(await container.get("obj2") === await container.get("obj2")) // true
     // endsection
-    turnConsoleOn()
 
-    expect(await container.get("obj1")).toEqual({message: "this is obj1"})
-    expect(await container.get("obj2")).toEqual({message: "this is obj2"})
-
-    expect(await container.get("obj1") === await container.get("obj1")).toBeTruthy()
-    expect(await container.get("obj2") === await container.get("obj2")).toBeTruthy()
+    expect(endConsoleCapture()).toEqual([
+      {message: "this is obj1"},
+      {message: "this is obj2"},
+      true,
+      true,
+    ])
   })
 
   it("test to bind promise value", async () => {
-    expect.assertions(4)
-
     const container = di.create()
 
-    turnConsoleOff()
+    startConsoleCapture()
+
     // section:bind-promise-value
     function promise1() {
       return new Promise(resolve => resolve({message: "this is promise1"}))
@@ -63,21 +48,19 @@ describe("tests for README", () => {
     console.log(await container.get("promise1") === await container.get("promise1")) // true
     console.log(await container.get("promise2") === await container.get("promise2")) // true
     // endsection
-    turnConsoleOn()
 
-    expect(await container.get("promise1")).toEqual({message: "this is promise1"})
-    expect(await container.get("promise2")).toEqual({message: "this is promise2"})
-
-    expect(await container.get("promise1") === await container.get("promise1")).toBeTruthy()
-    expect(await container.get("promise2") === await container.get("promise2")).toBeTruthy()
+    expect(endConsoleCapture()).toEqual([
+      {message: "this is promise1"},
+      {message: "this is promise2"},
+      true,
+      true,
+    ])
   })
 
   it("test to bind factory", async () => {
-    expect.assertions(3)
-
     const container = di.create()
 
-    turnConsoleOff()
+    startConsoleCapture()
     // section:bind-factory
     container.factory("factory1", () => ({message: "this is factory"}))
     container.factory("factory2", () => {
@@ -94,11 +77,12 @@ describe("tests for README", () => {
     console.log(await container.get("factory2")) // {message: "this is promise factory"}
     console.log(await container.get("factory3")) // {message: "this is async factory"}
     // endsection
-    turnConsoleOn()
 
-    expect(await container.get("factory1")).toEqual({message: "this is factory"})
-    expect(await container.get("factory2")).toEqual({message: "this is promise factory"})
-    expect(await container.get("factory3")).toEqual({message: "this is async factory"})
+    expect(endConsoleCapture()).toEqual([
+      {message: "this is factory"},
+      {message: "this is promise factory"},
+      {message: "this is async factory"},
+    ])
   })
 
   it("test to bind class", async () => {
@@ -106,7 +90,8 @@ describe("tests for README", () => {
 
     const container = di.create()
 
-    turnConsoleOff()
+    startConsoleCapture()
+
     // section:bind-class
     class Driver {
     }
@@ -122,21 +107,21 @@ describe("tests for README", () => {
     console.log(connection) // Connection { driver: Driver {} }
     console.log(connection.driver) // Driver {}
     // endsection
-    turnConsoleOn()
+
+    endConsoleCapture()
 
     expect(connection).toBeInstanceOf(Connection)
     expect(connection.driver).toBeInstanceOf(Driver)
   })
 
   it("test singleton descriptor", async () => {
-    expect.assertions(4)
-
     const container = di.create()
 
     class Foo {
     }
 
-    turnConsoleOff()
+    startConsoleCapture()
+
     // section:singleton-descriptor
     container.factory("factory.normal", () => ({message: "this is factory"}))
     container.factory("factory.singleton", () => ({message: "this is factory with singleton"})).singleton()
@@ -152,22 +137,20 @@ describe("tests for README", () => {
     console.log(await container.get("factory.singleton") === await container.get("factory.singleton")) // true
     console.log(await container.get("class.singleton") === await container.get("class.singleton")) // true
     // endsection
-    turnConsoleOn()
 
-    // equal, but not same.
-    expect(await container.get("factory.normal") === await container.get("factory.normal")).toBeFalsy()
-    expect(await container.get("class.normal") === await container.get("class.normal")).toBeFalsy()
-
-    expect(await container.get("factory.singleton") === await container.get("factory.singleton")).toBeTruthy()
-    expect(await container.get("class.singleton") === await container.get("class.singleton")).toBeTruthy()
+    expect(endConsoleCapture()).toEqual([
+      false,
+      false,
+      true,
+      true,
+    ])
   })
 
   it("test after descriptor", async () => {
-    expect.assertions(1)
-
     const container = di.create()
 
-    turnConsoleOff()
+    startConsoleCapture()
+
     // section:after-descriptor
 
     container
@@ -181,9 +164,9 @@ describe("tests for README", () => {
     console.log(await container.get("foo")) // {message: "this is origin maessage. and something appended."}
 
     // endsection
-    turnConsoleOn()
 
-
-    expect(await container.get("foo")).toEqual({message: "this is origin maessage. and something appended."})
+    expect(endConsoleCapture()).toEqual([
+      {message: "this is origin maessage. and something appended."},
+    ])
   })
 })
